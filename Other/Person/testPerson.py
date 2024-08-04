a128c4e032b17cdcadfebf1dd50b7513855ef26b
Chronomortis <113357733+Chronomortis@users.noreply.github.com>
2024-08-04 03:13:57 +0300

    Update testPerson.py
    Test cases for the added functions

--- Other/Person/testPerson.py
+++ Other/Person/testPerson.py
@@ -10,3 +10,10 @@ personObj = wrappedPerson.wPerson(name, age, address)
 
 #Update the age of the person
 personObj.updateA(21)
+
+#Print the address information of a given person
+wrappedPerson.LocationTracker(personObj)
+
+#Change the name information of a given person and return the previous name as a str object
+old_name = wrappedPerson.NameChange(personObj,"Efe")
+print(old_name)
