1168b90b604455d62ce0b8585a67d952bb8eea82
ErenOzilgili <146587029+ErenOzilgili@users.noreply.github.com>
2024-08-04 16:14:39 +0300

    Class Sample Wrapped with Cython

---
+++ Other/HeartRates/HeartRates.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include "HeartRates.h"
+using namespace std;
+
+HeartRates::HeartRates(string fN, string lN, int d, int m, int y)
+	: firstName{fN}, lastName{lN}, birthDay{d}, birthMonth{m}, birthYear{y}
+{}
+
+void HeartRates::setfirstName(string fN)
+{
+	firstName = fN;
+}
+
+string HeartRates::getfirstName()
+{
+	return firstName;
+}
+
+void HeartRates::setlastName(string lN)
+{
+	lastName = lN;
+}
+
+string HeartRates::getlastName()
+{
+	return lastName;
+}
+
+void HeartRates::setbirhtDay(int d)
+{
+	birthDay = d;
+}
+
+int HeartRates::getbirthDay()
+{
+	return birthDay;
+}
+
+void HeartRates::setbirthMonth(int m)
+{
+	birthMonth = m;
+}
+
+int HeartRates::getbirthMonth()
+{
+	return birthMonth;
+}
+
+void HeartRates::setbirthYear(int y)
+{
+	birthYear = y;
+}
+
+int HeartRates::getbirthYear()
+{
+	return birthYear;
+}
+
+void HeartRates::setDate()
+{
+	cout << "Please enter today's day, month, year: ";
+	cin >> currentDay >> currentMonth >> currentYear;
+}
+
+int HeartRates::getAge()
+{
+	if (currentMonth < birthMonth)
+	{
+		return currentYear - birthYear - 1;
+	}
+	else if (currentMonth == birthMonth)
+	{
+		if (currentDay < birthDay)
+		{
+			return currentYear - birthYear - 1;
+		}
+	}
+		return currentYear - birthYear;
+}
+
+int HeartRates::getMaximumHearthRate()
+{
+	return 220 - getAge();//this formula for calculating your maximum hearth rate.
+}
+
+int HeartRates::getMinimumTargetHearthRate()
+{
+	return 50 * getMaximumHearthRate() / 100;
+}
+
+int HeartRates::getMaximumTragetHearthRate()
+{
+	return 85 * getMaximumHearthRate() / 100;
+}
+
