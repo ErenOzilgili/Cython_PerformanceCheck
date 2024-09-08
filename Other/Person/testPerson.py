01d5bcaf8f1ed4a7781b52ab42cbf30747c541f3
Eren Özilgili <erenozilgili@gmail.com>
2024-09-08 21:07:52 +0300

    Comment on testPerson.py

--- Other/Person/testPerson.py
+++ Other/Person/testPerson.py
@@ -8,11 +8,17 @@ age = 20
 #Constructor
 personObj = wrappedPerson.wPerson(name, age, address)
 
+"""
+Normal functionality shown
+"""
 #Update the age of the person
 print("Updating age... with int = 21")
 personObj.updateA(21)
 personObj.display()
 
+"""
+Error thrown from cython and c++ respectively displayed above as examples
+"""
 print("\n------------------")
 print("Error thrown from cython itself:")
 print("Updating age... with string = \"a\"")
